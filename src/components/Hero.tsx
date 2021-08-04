19bac4ce4b757b2d00d9f59e8a614b98507913d3
Murilo D' Salvi Furlan <56124167+MuriloSalvi@users.noreply.github.com>
2021-08-04 11:32:15 -0300

    navbar and hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,44 @@
 import banner from "../assets/hero.jpg";
 import React from "react";
+import { faPlay, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Hero = ()=>{
+const Hero = ({title = 'Avengers Lixo', grade = 10 })=>{
+  
+const getBorderColor = ()=>{
+  if (grade === 10) {
+    return 'border-green-400 py-3'
+  } else if (grade >= 7){
+    return 'border-green-400 py-2'
+  } else if (grade >= 4 && grade <7 ){
+    return 'border-yellow-400 py-2'
+  } else {
+    return 'border-red-400 py-2'
+  }
+  
+}
+
   return(
-    <header className='min-h-screen'>
+    <header className='relative min-h-screen'>
       <img 
       className='w-full object-center background-cover'
        src={banner} 
        alt='banner_image'/>
+       <article className='absolute bottom-0 mb-64 ml-28'>
+         <p className='text-3xl'>Assista agora:</p>
+         <h2 className='text-6xl font-extrabold'>{title}</h2>
+         <p className='text-bas'>Nota <span className={`inline-block px-4 border-4 bg-black bg-opacity-75 rounded-full ${getBorderColor()}`}>{grade}</span></p>
+         
+         <button className='text-base mr-4 mt-8 py-2 px-8 roundede bg-black bg-opacity-50 transition-all duration-500
+            ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
+           <FontAwesomeIcon className='mr-2' icon={faPlay}/>  Assistir
+           </button>
+         <button className='text-base mr-4 mt-8 py-2 px-8 roundede bg-black bg-opacity-50 transition-all duration-500
+            ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
+         <FontAwesomeIcon className='mr-2' icon={faPlus}/> Minha Lista
+           </button>
+         
+       </article>
     </header>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
@@ -6,18 +6,23 @@ import logo from "../assets/imagiflix.png";
 import placeholderUser from "../assets/user.jpg";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className="absolute top-0 left-0 grid grid-cols-2 justify-between w-full p-8 ">
-      <div className="jusify-self-start grid grid-cols-2 items-center ">
+      <div className="jusify-self-start grid grid-cols-2 items-center ml-28">
         <h1 className="hidden">Imagiflix</h1>
         <img src={logo} alt="imagiflix" className="" />
 
         <ul className=" grid grid-flow-col gap-4 ">
-          <li className="font-bold">
+          <li className="font-bold hover:">
             <a href="#home">Inicio</a>
           </li>
-          <li >
-            <a  href="#series" >Séries</a>
+          <li>
+            <a href="#series">Séries</a>
           </li>
           <li className="hover:font-bold">
             <a href="#movies">Filmes</a>
@@ -25,7 +30,7 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className="justify-self-end flex justify-items-end items-center ">
+      <div className="justify-self-end flex justify-items-end items-center mr-28">
         <form className="relative w-64">
           <input
             className="w-full 
@@ -42,7 +47,6 @@ const Navbar = () => {
             hover:opacity-100
             focus:opacity-100
             "
-            
             type="text"
             placeholder="Titulos, gente e generos"
           />
@@ -51,16 +55,37 @@ const Navbar = () => {
           </button>
         </form>
 
-        <div className="flex">
-          <img src={placeholderUser} alt="usuario" className="ml-4" />
-          <button>
+        <div className="relative flex ml-4">
+          <img src={placeholderUser} alt="usuario" onClick={handleClick} />
+          <button onClick={handleClick}>
             <FontAwesomeIcon className="ml-2" icon={faCaretDown} />
           </button>
 
-          <ul className="hidden">
-            <li>Minha conta</li>
+          <ul
+            className={`
+            absolute
+            mt-10
+            p-1
+            w-32
+            right-0
+            bg-black
+            rounded 
+            transition-all
+            duration-500
+            ease-in-out
+            ${showMenu && "invisible opacity-0"}
+            `}
+            onClick={handleClick}
+          >
             <li>
-              <a href="#logout">sair</a>
+              <a href="#home" onClick={handleClick}>
+                Minha conta
+              </a>
+            </li>
+            <li>
+              <a href="#logout" onClick={handleClick}>
+                sair
+              </a>
             </li>
           </ul>
         </div>
