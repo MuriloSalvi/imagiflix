38b4fd30b84b492162bbbee5bde1e4676b2a09de
Murilo D' Salvi Furlan <murilo_salvi@outlook.com>
2021-08-30 11:14:07 -0300

    Corrections loading

--- src/components/LoadingModal.tsx
+++ src/components/LoadingModal.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import logo from '../assets/imagiflix.png'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import spinner from '../assets/spinner.svg'
 
 const LoadingModal = ()=>{
     return(
-        <>
-        <div className='flex items-center flex-col text-center w-screen h-screen bg-transparent'>
+        
+        <div className='flex items-center flex-col text-center w-screen h-screen bg-black'>
             <img className='w-1/3 py-24 z-50 ' src={logo} alt="logo" />
             
-            <FontAwesomeIcon className='z-50 ' icon={faSpinner} size='9x' color='red' pulse/>
-            <div className='w-full h-full fixed z-0 bg-gray-500 animate-pulse'>
+            <object type="image/svg+xml" data={spinner}>svg-animation</object>
+           
 
         </div>
             
-        </div>
         
-        </>
+        
     )
 }
 
