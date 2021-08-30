59b3abaec2fb1a798f99fe3e4dcc0c48d0cba79a
Murilo D' Salvi Furlan <murilo_salvi@outlook.com>
2021-08-30 11:17:53 -0300

    update loading

--- src/components/LoadingModal.tsx
+++ src/components/LoadingModal.tsx
@@ -1,21 +1,17 @@
-import React from 'react'
-import logo from '../assets/imagiflix.png'
-import spinner from '../assets/spinner.svg'
+import React from "react";
+import logo from "../assets/imagiflix.png";
+import spinner from "../assets/spinner.svg";
 
-const LoadingModal = ()=>{
-    return(
-        
-        <div className='flex items-center flex-col text-center w-screen h-screen bg-black'>
-            <img className='w-1/3 py-24 z-50 ' src={logo} alt="logo" />
-            
-            <object type="image/svg+xml" data={spinner}>svg-animation</object>
-           
+const LoadingModal = () => {
+  return (
+    <div className="flex items-center flex-col text-center w-screen h-screen bg-black">
+      <img className="w-1/3 py-24 z-50 " src={logo} alt="logo" />
 
-        </div>
-            
-        
-        
-    )
-}
+      <object type="image/svg+xml" data={spinner}>
+        svg-animation
+      </object>
+    </div>
+  );
+};
 
 export default LoadingModal;
